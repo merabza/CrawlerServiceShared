3df1f029a540bd0e786644207ad0ea6dbed0039e
merabza <merabza@gmail.com>
2026-06-26 07:41:52 +0400

    202606260739

--- CrawlerServiceShared.Contracts/TaskDto.cs
+++ CrawlerServiceShared.Contracts/TaskDto.cs
@@ -7,6 +7,5 @@ public sealed class TaskDto : ItemData
 {
     public int TaskId { get; set; }
     public string TaskName { get; set; } = string.Empty;
-    public string? ApiName { get; set; }
     public List<TaskStartPointDto> StartPoints { get; set; } = [];
 }
