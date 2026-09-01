75ffaa87db9090435eca31d9becb1538debbe3aa
merabza <merabza@gmail.com>
2026-09-01 20:27:13 +0400

    202609012027

--- CrawlerServiceShared.Contracts/CrawlerServiceApiClient.cs
+++ CrawlerServiceShared.Contracts/CrawlerServiceApiClient.cs
@@ -5,11 +5,9 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using CrawlerServiceShared.Contracts.V1.Routes;
-using LanguageExt;
 using Microsoft.Extensions.Logging;
-using OneOf;
 using SystemTools.ReCounterContracts;
-using SystemTools.SystemToolsShared.Errors;
+using SystemTools.SharedKernel;
 
 namespace CrawlerServiceShared.Contracts;
 
@@ -22,7 +20,7 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
     {
     }
 
-    public ValueTask<Option<ErrorOmd[]>> RunBatch(string batchName, int newPartsCreateLimit, int progressDelaySeconds,
+    public ValueTask<Result> RunBatch(string batchName, int newPartsCreateLimit, int progressDelaySeconds,
         CancellationToken cancellationToken = default)
     {
         return PostAsync(
@@ -31,7 +29,7 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             "&progressDelaySeconds=" + progressDelaySeconds, cancellationToken);
     }
 
-    public Task<OneOf<CrawlerPreCheckResult, ErrorOmd[]>> PreCheck(string name, string? pageAddress,
+    public Task<Result<CrawlerPreCheckResult>> PreCheck(string name, string? pageAddress,
         CancellationToken cancellationToken = default)
     {
         string afterServerAddress = CrawlerServiceApiRoutes.CrawlerRoute.CrawlerBase +
@@ -42,14 +40,14 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
         return GetAsyncReturn<CrawlerPreCheckResult>(afterServerAddress, false, cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> RunTask(RunTaskRequest request, CancellationToken cancellationToken = default)
+    public ValueTask<Result> RunTask(RunTaskRequest request, CancellationToken cancellationToken = default)
     {
         return PostAsync(
             CrawlerServiceApiRoutes.CrawlerRoute.CrawlerBase + CrawlerServiceApiRoutes.CrawlerRoute.RunTask, true,
             JsonSerializer.Serialize(request), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> TestOnePage(TestOnePageRequest request,
+    public ValueTask<Result> TestOnePage(TestOnePageRequest request,
         CancellationToken cancellationToken = default)
     {
         return PostAsync(
@@ -57,43 +55,42 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             JsonSerializer.Serialize(request), cancellationToken);
     }
 
-    public Task<OneOf<List<BatchDto>, ErrorOmd[]>> GetBatchesList(CancellationToken cancellationToken = default)
+    public Task<Result<List<BatchDto>>> GetBatchesList(CancellationToken cancellationToken = default)
     {
         return GetAsyncReturn<List<BatchDto>>(
             CrawlerServiceApiRoutes.BatchRoute.BatchBase + CrawlerServiceApiRoutes.BatchRoute.List, false,
             cancellationToken);
     }
 
-    public async Task<OneOf<BatchDto?, ErrorOmd[]>> GetBatchByName(string batchName,
+    public Task<Result<BatchDto>> GetBatchByName(string batchName,
         CancellationToken cancellationToken = default)
     {
-        OneOf<ApiNullableResult<BatchDto>, ErrorOmd[]> result = await GetAsyncReturn<ApiNullableResult<BatchDto>>(
+        return GetAsyncReturn<BatchDto>(
             CrawlerServiceApiRoutes.BatchRoute.BatchBase + CrawlerServiceApiRoutes.BatchRoute.GetByName + "/?name=" +
             Uri.EscapeDataString(batchName), false, cancellationToken);
-        return result.Match<OneOf<BatchDto?, ErrorOmd[]>>(wrapper => wrapper.Value, errors => errors);
     }
 
-    public Task<OneOf<BatchDto, ErrorOmd[]>> CreateBatch(BatchDto batch, CancellationToken cancellationToken = default)
+    public Task<Result<BatchDto>> CreateBatch(BatchDto batch, CancellationToken cancellationToken = default)
     {
         return PostAsyncReturn<BatchDto>(
             CrawlerServiceApiRoutes.BatchRoute.BatchBase + CrawlerServiceApiRoutes.BatchRoute.Create, false,
             JsonSerializer.Serialize(batch), cancellationToken);
     }
 
-    public Task<Option<ErrorOmd[]>> UpdateBatch(BatchDto batch, CancellationToken cancellationToken = default)
+    public Task<Result> UpdateBatch(BatchDto batch, CancellationToken cancellationToken = default)
     {
         return PutAsync(CrawlerServiceApiRoutes.BatchRoute.BatchBase + CrawlerServiceApiRoutes.BatchRoute.Update,
             JsonSerializer.Serialize(batch), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> DeleteBatch(string batchName, CancellationToken cancellationToken = default)
+    public ValueTask<Result> DeleteBatch(string batchName, CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
             CrawlerServiceApiRoutes.BatchRoute.BatchBase + CrawlerServiceApiRoutes.BatchRoute.Delete + "/?name=" +
             Uri.EscapeDataString(batchName), cancellationToken);
     }
 
-    public Task<OneOf<List<string>, ErrorOmd[]>> GetHostStartUrlNamesByBatch(string batchName,
+    public Task<Result<List<string>>> GetHostStartUrlNamesByBatch(string batchName,
         CancellationToken cancellationToken = default)
     {
         return GetAsyncReturn<List<string>>(
@@ -101,7 +98,7 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             "/?batchName=" + Uri.EscapeDataString(batchName), false, cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> AddHostByBatch(HostByBatchRequest request,
+    public ValueTask<Result> AddHostByBatch(HostByBatchRequest request,
         CancellationToken cancellationToken = default)
     {
         return PostAsync(
@@ -109,7 +106,7 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             JsonSerializer.Serialize(request), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> RemoveHostByBatch(string batchName, string schemeName, string hostName,
+    public ValueTask<Result> RemoveHostByBatch(string batchName, string schemeName, string hostName,
         CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
@@ -118,59 +115,59 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             "&hostName=" + Uri.EscapeDataString(hostName), cancellationToken);
     }
 
-    public Task<OneOf<List<HostDto>, ErrorOmd[]>> GetHostsList(CancellationToken cancellationToken = default)
+    public Task<Result<List<HostDto>>> GetHostsList(CancellationToken cancellationToken = default)
     {
         return GetAsyncReturn<List<HostDto>>(
             CrawlerServiceApiRoutes.HostRoute.HostBase + CrawlerServiceApiRoutes.HostRoute.List, false,
             cancellationToken);
     }
 
-    public async Task<OneOf<HostDto?, ErrorOmd[]>> GetHostByName(string hostName,
+    public async Task<Result<HostDto?>> GetHostByName(string hostName,
         CancellationToken cancellationToken = default)
     {
-        OneOf<ApiNullableResult<HostDto>, ErrorOmd[]> result = await GetAsyncReturn<ApiNullableResult<HostDto>>(
+        Result<ApiNullableResult<HostDto>> result = await GetAsyncReturn<ApiNullableResult<HostDto>>(
             CrawlerServiceApiRoutes.HostRoute.HostBase + CrawlerServiceApiRoutes.HostRoute.GetByName + "/?name=" +
             Uri.EscapeDataString(hostName), false, cancellationToken);
-        return result.Match<OneOf<HostDto?, ErrorOmd[]>>(wrapper => wrapper.Value, errors => errors);
+        return result.IsFailure ? Result.Failure<HostDto?>(result.Error) : Result.Success(result.Value.Value);
     }
 
-    public Task<OneOf<HostDto, ErrorOmd[]>> CreateHost(HostDto host, CancellationToken cancellationToken = default)
+    public Task<Result<HostDto>> CreateHost(HostDto host, CancellationToken cancellationToken = default)
     {
         return PostAsyncReturn<HostDto>(
             CrawlerServiceApiRoutes.HostRoute.HostBase + CrawlerServiceApiRoutes.HostRoute.Create, false,
             JsonSerializer.Serialize(host), cancellationToken);
     }
 
-    public Task<Option<ErrorOmd[]>> UpdateHost(HostDto host, CancellationToken cancellationToken = default)
+    public Task<Result> UpdateHost(HostDto host, CancellationToken cancellationToken = default)
     {
         return PutAsync(CrawlerServiceApiRoutes.HostRoute.HostBase + CrawlerServiceApiRoutes.HostRoute.Update,
             JsonSerializer.Serialize(host), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> DeleteHost(string hostName, CancellationToken cancellationToken = default)
+    public ValueTask<Result> DeleteHost(string hostName, CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
             CrawlerServiceApiRoutes.HostRoute.HostBase + CrawlerServiceApiRoutes.HostRoute.Delete + "/?name=" +
             Uri.EscapeDataString(hostName), cancellationToken);
     }
 
-    public Task<OneOf<List<SchemeDto>, ErrorOmd[]>> GetSchemesList(CancellationToken cancellationToken = default)
+    public Task<Result<List<SchemeDto>>> GetSchemesList(CancellationToken cancellationToken = default)
     {
         return GetAsyncReturn<List<SchemeDto>>(
             CrawlerServiceApiRoutes.SchemeRoute.SchemeBase + CrawlerServiceApiRoutes.SchemeRoute.List, false,
             cancellationToken);
     }
 
-    public async Task<OneOf<SchemeDto?, ErrorOmd[]>> GetSchemeByName(string schemeName,
+    public async Task<Result<SchemeDto?>> GetSchemeByName(string schemeName,
         CancellationToken cancellationToken = default)
     {
-        OneOf<ApiNullableResult<SchemeDto>, ErrorOmd[]> result = await GetAsyncReturn<ApiNullableResult<SchemeDto>>(
+        Result<ApiNullableResult<SchemeDto>> result = await GetAsyncReturn<ApiNullableResult<SchemeDto>>(
             CrawlerServiceApiRoutes.SchemeRoute.SchemeBase + CrawlerServiceApiRoutes.SchemeRoute.GetByName + "/?name=" +
             Uri.EscapeDataString(schemeName), false, cancellationToken);
-        return result.Match<OneOf<SchemeDto?, ErrorOmd[]>>(wrapper => wrapper.Value, errors => errors);
+        return result.IsFailure ? Result.Failure<SchemeDto?>(result.Error) : Result.Success(result.Value.Value);
     }
 
-    public Task<OneOf<SchemeDto, ErrorOmd[]>> CreateScheme(SchemeDto scheme,
+    public Task<Result<SchemeDto>> CreateScheme(SchemeDto scheme,
         CancellationToken cancellationToken = default)
     {
         return PostAsyncReturn<SchemeDto>(
@@ -178,56 +175,56 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             JsonSerializer.Serialize(scheme), cancellationToken);
     }
 
-    public Task<Option<ErrorOmd[]>> UpdateScheme(SchemeDto scheme, CancellationToken cancellationToken = default)
+    public Task<Result> UpdateScheme(SchemeDto scheme, CancellationToken cancellationToken = default)
     {
         return PutAsync(CrawlerServiceApiRoutes.SchemeRoute.SchemeBase + CrawlerServiceApiRoutes.SchemeRoute.Update,
             JsonSerializer.Serialize(scheme), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> DeleteScheme(string schemeName, CancellationToken cancellationToken = default)
+    public ValueTask<Result> DeleteScheme(string schemeName, CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
             CrawlerServiceApiRoutes.SchemeRoute.SchemeBase + CrawlerServiceApiRoutes.SchemeRoute.Delete + "/?name=" +
             Uri.EscapeDataString(schemeName), cancellationToken);
     }
 
-    public Task<OneOf<List<TaskDto>, ErrorOmd[]>> GetTasksList(CancellationToken cancellationToken = default)
+    public Task<Result<List<TaskDto>>> GetTasksList(CancellationToken cancellationToken = default)
     {
         return GetAsyncReturn<List<TaskDto>>(
             CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.List, false,
             cancellationToken);
     }
 
-    public async Task<OneOf<TaskDto?, ErrorOmd[]>> GetTaskByName(string taskName,
+    public async Task<Result<TaskDto?>> GetTaskByName(string taskName,
         CancellationToken cancellationToken = default)
     {
-        OneOf<ApiNullableResult<TaskDto>, ErrorOmd[]> result = await GetAsyncReturn<ApiNullableResult<TaskDto>>(
+        Result<ApiNullableResult<TaskDto>> result = await GetAsyncReturn<ApiNullableResult<TaskDto>>(
             CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.GetByName + "/?name=" +
             Uri.EscapeDataString(taskName), false, cancellationToken);
-        return result.Match<OneOf<TaskDto?, ErrorOmd[]>>(wrapper => wrapper.Value, errors => errors);
+        return result.IsFailure ? Result.Failure<TaskDto?>(result.Error) : Result.Success(result.Value.Value);
     }
 
-    public Task<OneOf<TaskDto, ErrorOmd[]>> CreateTask(TaskDto task, CancellationToken cancellationToken = default)
+    public Task<Result<TaskDto>> CreateTask(TaskDto task, CancellationToken cancellationToken = default)
     {
         return PostAsyncReturn<TaskDto>(
             CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.Create, false,
             JsonSerializer.Serialize(task), cancellationToken);
     }
 
-    public Task<Option<ErrorOmd[]>> UpdateTask(TaskDto task, CancellationToken cancellationToken = default)
+    public Task<Result> UpdateTask(TaskDto task, CancellationToken cancellationToken = default)
     {
         return PutAsync(CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.Update,
             JsonSerializer.Serialize(task), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> DeleteTask(string taskName, CancellationToken cancellationToken = default)
+    public ValueTask<Result> DeleteTask(string taskName, CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
             CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.Delete + "/?name=" +
             Uri.EscapeDataString(taskName), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> ClearTaskFetchedData(string taskName,
+    public ValueTask<Result> ClearTaskFetchedData(string taskName,
         CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
@@ -235,17 +232,17 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             "/?name=" + Uri.EscapeDataString(taskName), cancellationToken);
     }
 
-    public async Task<OneOf<TaskStartPointDto?, ErrorOmd[]>> GetStartPoint(int taskId, string startPoint,
+    public async Task<Result<TaskStartPointDto?>> GetStartPoint(int taskId, string startPoint,
         CancellationToken cancellationToken = default)
     {
-        OneOf<ApiNullableResult<TaskStartPointDto>, ErrorOmd[]> result =
+        Result<ApiNullableResult<TaskStartPointDto>> result =
             await GetAsyncReturn<ApiNullableResult<TaskStartPointDto>>(
                 CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.StartPointGet +
                 "/?taskId=" + taskId + "&startPoint=" + Uri.EscapeDataString(startPoint), false, cancellationToken);
-        return result.Match<OneOf<TaskStartPointDto?, ErrorOmd[]>>(wrapper => wrapper.Value, errors => errors);
+        return result.IsFailure ? Result.Failure<TaskStartPointDto?>(result.Error) : Result.Success(result.Value.Value);
     }
 
-    public Task<OneOf<TaskStartPointDto, ErrorOmd[]>> AddStartPoint(AddStartPointRequest request,
+    public Task<Result<TaskStartPointDto>> AddStartPoint(AddStartPointRequest request,
         CancellationToken cancellationToken = default)
     {
         return PostAsyncReturn<TaskStartPointDto>(
@@ -253,14 +250,14 @@ public sealed class CrawlerServiceApiClient : ReCounterApiClient
             JsonSerializer.Serialize(request), cancellationToken);
     }
 
-    public Task<Option<ErrorOmd[]>> UpdateStartPoint(TaskStartPointDto startPoint,
+    public Task<Result> UpdateStartPoint(TaskStartPointDto startPoint,
         CancellationToken cancellationToken = default)
     {
         return PutAsync(CrawlerServiceApiRoutes.TaskRoute.TaskBase + CrawlerServiceApiRoutes.TaskRoute.StartPointUpdate,
             JsonSerializer.Serialize(startPoint), cancellationToken);
     }
 
-    public ValueTask<Option<ErrorOmd[]>> DeleteStartPoint(int taskId, string startPoint,
+    public ValueTask<Result> DeleteStartPoint(int taskId, string startPoint,
         CancellationToken cancellationToken = default)
     {
         return DeleteAsync(
--- CrawlerServiceShared.Contracts/Errors/CrawlerServiceErrors.cs
+++ CrawlerServiceShared.Contracts/Errors/CrawlerServiceErrors.cs
@@ -1,47 +1,37 @@
-using SystemTools.SystemToolsShared.Errors;
+using SystemTools.SharedKernel;
 
 namespace CrawlerServiceShared.Contracts.Errors;
 
 public static class CrawlerServiceErrors
 {
-    public static readonly ErrorOmd CrawlAlreadyRunning = new()
-    {
-        Code = nameof(CrawlAlreadyRunning), Name = "Crawl process is already running"
-    };
+    public static Error CrawlAlreadyRunning =>
+        Error.Conflict(nameof(CrawlAlreadyRunning), "Crawl process is already running");
 
-    public static readonly ErrorOmd ParseOnePageParametersNotCreated = new()
-    {
-        Code = nameof(ParseOnePageParametersNotCreated), Name = "ParseOnePageParameters does not created"
-    };
+    public static Error ParseOnePageParametersNotCreated =>
+        Error.Problem(nameof(ParseOnePageParametersNotCreated), "ParseOnePageParameters does not created");
 
-    public static ErrorOmd BatchWithNameNotFound(string batchName)
+    public static Error BatchWithNameNotFound(string batchName)
     {
-        return new ErrorOmd { Code = nameof(BatchWithNameNotFound), Name = $"Batch with name {batchName} not found" };
+        return Error.NotFound(nameof(BatchWithNameNotFound), $"Batch with name {batchName} not found");
     }
 
-    public static ErrorOmd TaskWithNameNotFound(string? taskName)
+    public static Error TaskWithNameNotFound(string? taskName)
     {
-        return new ErrorOmd { Code = nameof(TaskWithNameNotFound), Name = $"Task with name {taskName} not found" };
+        return Error.NotFound(nameof(TaskWithNameNotFound), $"Task with name {taskName} not found");
     }
 
-    public static ErrorOmd HostWithNameNotFound(string? hostName)
+    public static Error HostWithNameNotFound(string? hostName)
     {
-        return new ErrorOmd { Code = nameof(HostWithNameNotFound), Name = $"Host with name {hostName} not found" };
+        return Error.NotFound(nameof(HostWithNameNotFound), $"Host with name {hostName} not found");
     }
 
-    public static ErrorOmd SchemeWithNameNotFound(string? schemeName)
+    public static Error SchemeWithNameNotFound(string? schemeName)
     {
-        return new ErrorOmd
-        {
-            Code = nameof(SchemeWithNameNotFound), Name = $"Scheme with name {schemeName} not found"
-        };
+        return Error.NotFound(nameof(SchemeWithNameNotFound), $"Scheme with name {schemeName} not found");
     }
 
-    public static ErrorOmd StartPointNotFound(int taskId, string? startPoint)
+    public static Error StartPointNotFound(int taskId, string? startPoint)
     {
-        return new ErrorOmd
-        {
-            Code = nameof(StartPointNotFound), Name = $"Start point {startPoint} for task id {taskId} not found"
-        };
+        return Error.NotFound(nameof(StartPointNotFound), $"Start point {startPoint} for task id {taskId} not found");
     }
 }
